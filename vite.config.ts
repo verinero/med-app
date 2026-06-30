ee3292521241f51128b3dbec07a4824b55f62ac7
Tiffany Laij <tiffanylaij@Tiffanys-MacBook-Pro.local>
2026-06-30 00:53:55 -0400

    workflow to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/med-app/",
   plugins: [
     react(),
     tailwindcss(),
